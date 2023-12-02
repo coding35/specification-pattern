38f847a8f757529dcf5228d92125ee20110d24d3
adam <imamreed@outlook.com>
2023-12-02 09:44:09 -0600

    restructure

--- Model/Order.cs
+++
@@ -1,8 +0,0 @@
-    namespace SpecificationPattern.Model
-    {
-        public class Order
-        {
-            public decimal TotalAmount { get; set; }
-            public decimal TotalWeight { get; set; }
-        }
-    }
--- Program.cs
+++
@@ -1,7 +0,0 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("test");
-    }
-}
